7b30a09cddbf85fb537dca7d8978050bcd54c820
EthanDaviesBSU <146755001+EthanDaviesBSU@users.noreply.github.com>
2024-04-22 16:41:34 +0100

    Update Movement.cs

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float moveSpeed;
+    public float jumpVelocity;
+    public bool isJump = false;
     public Transform target;
+    private Rigidbody2D _RigidBody;
     // Start is called before the first frame update
     void Start()
     {
-
+        _RigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,8 +23,23 @@ public class Movement : MonoBehaviour
             transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime * moveSpeed, Space.World);
         }
         if (Input.GetKey("d"))
+
         {
             transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * moveSpeed, Space.World);
         }
+
+        if (Input.GetKey ("space") && !isJump)
+        {
+            _RigidBody.AddForce(Vector2.up * jumpVelocity);
+                isJump = true;
+        }
     }
+    
+    void OnCollisionEnter2D(Collision2D collision)
+        {
+            if(collision.gameObject.tag == "Floor")
+            {
+                isJump = false;
+            }
+        }
 }
